ccb9245f6ab27b646b96c7f0a8a17758b02a4840
Alex Miller <alexandermi@alexandermi-macbookpro.roam.corp.google.com>
2014-04-05 15:00:33 -0700

    added threading to main class

--- source/apg/ragdoll/server/Game.hx
+++ source/apg/ragdoll/server/Game.hx
@@ -30,6 +30,6 @@ class Game {
   }
 
   public function broadcastToPlayers(message) : Void {
-
+    
   }
 }
--- source/apg/ragdoll/server/Main.hx
+++ source/apg/ragdoll/server/Main.hx
@@ -2,24 +2,43 @@ package apg.ragdoll.server;
 
 import sys.net.Socket;
 import sys.net.Host;
+import neko.vm.Thread;
 
 import apg.ragdoll.server.states.WaitForPlayers;
 
+enum GameThreadState {
+  NeedsConnections;
+  DoesNotNeedConnections;
+}
+
 class Main {
   public static function main() {
-    var game : Game = new Game(1);
-    game.setState(new WaitForPlayers());
-
     var s = new Socket();
     s.bind(new Host('localhost'), 9999);
     s.listen(1);
-    while (game.needsPlayers()) {
-      try {
+    while (true) {
+      var gameThread = Thread.create(startNewGame);
+      gameThread.sendMessage(Thread.current());
+      while (Thread.readMessage(true) == GameThreadState.NeedsConnections) {
         var clientConnection = s.accept();
+        gameThread.sendMessage(clientConnection);
+      }
+    }
+  }
+
+  private static function startNewGame() {
+    var parentThread : Thread = Thread.readMessage(true);
+    var game : Game = new Game(1);
+    game.setState(new WaitForPlayers());
+    while (game.needsPlayers()) {
+      parentThread.sendMessage(GameThreadState.NeedsConnections);
+      try {
+        var clientConnection : Socket = Thread.readMessage(true);
         game.addPlayer(new Player(clientConnection));
       } catch (error : String) {
         trace(error);
       }
     }
+    parentThread.sendMessage(GameThreadState.DoesNotNeedConnections);
   }
 }
